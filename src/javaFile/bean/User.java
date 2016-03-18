16c010611516bdf3813a33b7b5bc3254f130f897
purpletianjing <TianjingSun@outlook.com>
2016-03-18 14:51:45 +0800

    add user bean

---
+++ src/javaFile/bean/User.java
@@ -0,0 +1,29 @@
+package javaFile.bean;
+
+public class User {
+    public String name;
+    public int age;
+    public String gender;
+    public int id;
+    public String email;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
