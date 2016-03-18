9b75b3952d378c5625dbf276011fb401f3de5ef7
purpletianjing <TianjingSun@outlook.com>
2016-03-18 14:19:38 +0800

    read json

--- src/javaFile/Generator.java
+++ src/javaFile/Generator.java
@@ -1,6 +1,14 @@
 package javaFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Generator {
 
@@ -10,10 +18,12 @@ public class Generator {
         }
     }
 
-    public void checkJson(final File directory) {
+    public void checkJson(final File directory) throws IOException, ParseException {
+        Generator generator = new Generator();
         File[] fList = directory.listFiles();
 
         for (File file : fList) {
+            generator.readJson(file);
             String fileName  = file.getName();
             if (file.isFile() && (fileName.substring(fileName.lastIndexOf(".") + 1)).equals("json")) {
                 System.out.println("this is a json file");
@@ -21,7 +31,24 @@ public class Generator {
         }
     }
 
-    public static void main(String[] args) {
+    public void readJson(File jsonFile) throws IOException, ParseException{
+        JSONParser parser = new JSONParser();
+        JSONArray jsonArray = (JSONArray)parser.parse(new FileReader(jsonFile));
+
+        for (Object o : jsonArray) {
+            JSONObject person = (JSONObject) o;
+            String name = (String) person.get("name");
+            long age = (Long) person.get("age");
+            String gender = (String) person.get("gender");
+            String email = (String) person.get("email");
+
+            System.out.println("name: " + name + "age: " + age + "gender" + gender + "email" + email);
+
+        }
+
+  }
+
+    public static void main(String[] args) throws IOException, ParseException {
         Generator generator = new Generator();
         File directory = new File("/Users/twer/works/generate-logic-puzzle/seeds");
         generator.operateDirectory(directory);
