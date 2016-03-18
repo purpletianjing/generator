02d6e09883d653aeb0bfb86dd4f17c137fdd34b9
purpletianjing <TianjingSun@outlook.com>
2016-03-18 11:32:52 +0800

    check json

--- src/javaFile/Generator.java
+++ src/javaFile/Generator.java
@@ -1,19 +1,31 @@
 package javaFile;
 
 import java.io.File;
-import java.nio.file.Paths;
 
 public class Generator {
 
-    public void operateDirectory(final File folder) {
-        if (folder.exists() && folder.isDirectory()) {
+    public void operateDirectory(final File directory) {
+        if (directory.exists() && directory.isDirectory()) {
             System.out.println("this is an existing directory");
+        }
+    }
+
+    public void checkJson(final File directory) {
+        File[] fList = directory.listFiles();
+
+        for (File file : fList) {
+            String fileName  = file.getName();
+            if (file.isFile() && (fileName.substring(fileName.lastIndexOf(".") + 1)).equals("json")) {
+                System.out.println("this is a json file");
+            }
         }
     }
 
     public static void main(String[] args) {
         Generator generator = new Generator();
-        generator.operateDirectory(new File("/Users/twer/works/generate-logic-puzzle/seeds"));
+        File directory = new File("/Users/twer/works/generate-logic-puzzle/seeds");
+        generator.operateDirectory(directory);
+        generator.checkJson(directory);
     }
 
 }
